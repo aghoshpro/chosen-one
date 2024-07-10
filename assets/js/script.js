13f85942e756bbfe6b803b31899bd829cc23d69c
aghoshpro <aghoshpro@gmail.com>
2024-07-10 17:56:14 +0200

    Updates: back to top, coming soon, raster added

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,3 +185,21 @@ btn.addEventListener("click", function () {
   }
   localStorage.setItem("theme", theme);
 });
+
+/**
+ * back to top & header
+ */
+
+const header = document.querySelector("[data-header]");
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
